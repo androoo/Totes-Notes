8b7a959db61165b6c193864490fe3424c539fd44
Andrew Gierke <andrew.gierke@gmail.com>
2017-02-03 19:48:37 -0700

    Note Model and Note ModelController files created

--- TotesNotes/Note.swift
+++ TotesNotes/Note.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
-class Note {
+class Note: Equatable {
     
+    
+    //MARK: - Properties
+    var note: String
+    fileprivate static let noteKey = "note"
+    
+    init(note: String) {
+        self.note = note
+    }
+    
+    //MARK: - failable initializer
+    //this is taking a dict and guarding it. Then it sets the property
+    convenience init?(dictionary: [String: String]) {
+        guard let note = dictionary[Note.noteKey] else { return nil }
+        self.init(note: note)
+    }
+    
+    //MARK: - dictionary representation 
+    var dictionaryRepresentaiton: [String: String] {
+        return [Note.noteKey: note]
+    }
+    
+}
+
+//MARK: - conform to equitable
+
+func ==(lhs: Note, rhs: Note) -> Bool {
+    return lhs.note == rhs.note
 }
--- TotesNotes/NoteController.swift
+++ TotesNotes/NoteController.swift
@@ -10,4 +10,55 @@ import Foundation
 
 class NoteController {
     
+    //MARK: - Properties 
+    //singleton to give access to all these object answers
+    //empty array of notes initialized 
+    //some key i think 
+    
+    static let shared = NoteController()
+    var notesArray = [Note]()
+    fileprivate static let myNotesKey = "notes"
+    
+    //MARK: - initialize object with load func
+    init() {
+        loadFromPersistentStorage()
+    }
+    
+    
+    //MARK: - CRUD
+    
+    //add func
+    func add(note: Note) {
+        notesArray.append(note)
+        saveToPersistentStorage()
+    }
+    
+    //remove func
+    func remove(note: Note) {
+        guard let index = notesArray.index(of: note) else { return }
+        notesArray.remove(at: index)
+        saveToPersistentStorage()
+    }
+    
+    
+    //MARK: - Persistence
+    
+    //save func
+    func saveToPersistentStorage() {
+        let userDefaults = UserDefaults.standard
+        let notesDictionary = notesArray.map { $0.dictionaryRepresentaiton }
+        userDefaults.set(notesDictionary, forKey: NoteController.myNotesKey)
+    }
+    
+    //load func
+    func loadFromPersistentStorage() {
+        //persistent storage instance
+        let userDefaults = UserDefaults.standard
+        //create dict prop by looking in user defaults objects with a key as types
+        guard let notesDictionary = userDefaults.object(forKey: NoteController.myNotesKey) as? [[String: String]] else { return }
+        // set notes array to the flatmapped dictionary
+        notesArray = notesDictionary.flatMap { Note(dictionary: $0) }
+    }
+    
+    
 }
